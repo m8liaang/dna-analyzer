c348a0649240b1a36ab81ff8e398539945f57dfb
Michelle <michelle8.liang@gmail.com>
2026-07-17 20:05:20 +0400

    Create gc.py

---
+++ src/gc.py
@@ -0,0 +1,7 @@
+def GC_content(sequence):
+  G_counts = sequence.count('G')
+  C_counts = sequence.count('C')
+
+  GC_content = ((G_counts + C_counts) / len(sequence)) * 100
+
+  return print(f"\nGC Content: {round(GC_content, 2)}%")
